78a6baf1994cff31a97f758e40a22687c3e46799
mattfadini <mfrparada2704@gmail.com>
2023-05-30 17:21:52 -0300

    Criada a conexão com o banco de dados

---
+++ Sistema-gestao-estudantes/MEU_BD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Sistema_gestao_estudantes
+{
+    internal class MEU_BD
+    {
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=t4_sga_bd");
+    }
+}
